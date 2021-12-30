90c58628372d4036a94224cb88c9e8dd8c5e92b2
mariotmc <mariotomicx@gmail.com>
2021-12-30 19:59:34 +0100

    Add components, basic style and layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import General from "./components/General";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import PDFButton from "./components/PDFButton";
+import ResetButton from "./components/ResetButton";
+import Template from "./components/Template";
+import Footer from "./components/Footer";
+import "./styles/style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <main>
+          <form>
+            <General />
+            <Experience />
+            <Education />
+            <PDFButton />
+            <ResetButton />
+          </form>
+          <Template />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
